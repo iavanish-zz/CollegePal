083d945d7a5c62d080206649c9472266ae607563
iavanish <avanish1404@iiitd.ac.in>
2015-03-27 22:37:18 +0530

    Some Progress

---
+++ CollegePal/app/src/main/java/iavanish/collegepal/Courses/SelectCourse.java
@@ -0,0 +1,31 @@
+
+package iavanish.collegepal.Courses;
+
+import iavanish.collegepal.CommonClasses.DataBaseWrite;
+import iavanish.collegepal.CommonClasses.Student;
+
+/**
+ *
+ * @author iavanish
+ */
+
+/**
+ * 
+ * User selects a course to get enrolled into
+ */
+
+public class SelectCourse {
+    
+    private ShowCourses showCourses;
+    
+    private DataBaseWrite dataBaseWrite;
+    
+    public SelectCourse() {
+        
+    }
+    
+    public void selectCourse(Student student) {
+        
+    }
+    
+}
---
+++ CollegePal/app/src/main/java/iavanish/collegepal/Courses/UpdateExistingCourse.java
@@ -0,0 +1,31 @@
+
+package iavanish.collegepal.Courses;
+
+import iavanish.collegepal.CommonClasses.Course;
+import iavanish.collegepal.CommonClasses.DataBaseWrite;
+
+/**
+ *
+ * @author iavanish
+ */
+
+/**
+ * 
+ * Update an existing course
+ */
+
+public class UpdateExistingCourse {
+    
+    protected Course course;
+    
+    protected DataBaseWrite dataBaseWrite;
+    
+    public UpdateExistingCourse(Course course) {
+        this.course = course;
+    }
+    
+    public void updateCourse() {
+        
+    }
+    
+}
