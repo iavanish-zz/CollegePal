3005a1158343e7ad21d6ddfa0cbd2e42c8d9b48a
iavanish <avanish1404@iiitd.ac.in>
2015-03-27 22:39:45 +0530

    Some Progress

---
+++ CollegePal/app/src/main/java/iavanish/collegepal/Resource/PublicResource.java
@@ -0,0 +1,20 @@
+
+package iavanish.collegepal.Resource;
+
+/**
+ *
+ * @author iavanish
+ */
+
+/**
+ * 
+ * Public resources for a course
+ */
+
+public class PublicResource extends Resource {
+    
+    public PublicResource() {
+        super();
+    }
+    
+}
---
+++ CollegePal/app/src/main/java/iavanish/collegepal/Resource/Resource.java
@@ -0,0 +1,29 @@
+
+package iavanish.collegepal.Resource;
+
+import iavanish.collegepal.CommonClasses.Student;
+import iavanish.collegepal.CommonClasses.Course;
+
+/**
+ *
+ * @author iavanish
+ */
+
+/**
+ * 
+ * Resources for a course
+ */
+
+public class Resource {
+    
+    public Course course;
+    public Student student;
+    public String description;
+    public TypeOfResource typeOfResource;
+    public PublicPrivateResource publicPrivateResource;
+    
+    public Resource() {
+        
+    }
+    
+}
