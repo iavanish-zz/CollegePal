4703af1ab290436e4e8e7c8e534ce76b1a66db59
iavanish <avanish1404@iiitd.ac.in>
2015-03-27 22:29:32 +0530

    Some Progress

---
+++ CollegePal/app/src/main/java/iavanish/collegepal/Deadlines/Deadline.java
@@ -0,0 +1,30 @@
+
+package iavanish.collegepal.Deadlines;
+
+import iavanish.collegepal.CommonClasses.Date;
+import iavanish.collegepal.CommonClasses.Student;
+import iavanish.collegepal.CommonClasses.Course;
+
+/**
+ *
+ * @author iavanish
+ */
+
+/**
+ * 
+ * Deadlines
+ */
+
+public class Deadline {
+    
+    protected Course course;
+    protected Date date;
+    protected String details;
+    protected Student student;
+    protected PublicPrivateDeadline publicPrivateDeadline;
+    
+    public Deadline() {
+        
+    }
+    
+}
---
+++ CollegePal/app/src/main/java/iavanish/collegepal/Deadlines/PrivateDeadline.java
@@ -0,0 +1,28 @@
+
+package iavanish.collegepal.Deadlines;
+
+import iavanish.collegepal.CommonClasses.DataBaseWrite;
+
+/**
+ *
+ * @author iavanish
+ */
+
+/**
+ * 
+ * Private Deadlines
+ */
+
+public class PrivateDeadline extends Deadline {
+    
+    private DataBaseWrite dataBaseWrite;
+    
+    public PrivateDeadline() {
+        super();
+    }
+    
+    public void enterPrivateDeadline() {
+        
+    }
+    
+}
