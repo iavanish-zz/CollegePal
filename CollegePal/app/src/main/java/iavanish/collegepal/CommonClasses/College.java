03823f178c40355ef08b2ecedee498170340ea67
iavanish <avanish1404@iiitd.ac.in>
2015-03-27 20:30:08 +0530

    Some Progress

---
+++ CollegePal/app/src/main/java/iavanish/collegepal/CommonClasses/College.java
@@ -0,0 +1,23 @@
+
+package iavanish.collegepal.CommonClasses;
+
+/**
+ *
+ * @author iavanish
+ */
+
+/**
+ * 
+ * College
+ */
+
+public class College {
+    
+    public String collegeID;
+    public String collegeName;
+    
+    public College() {
+        
+    }
+    
+}
