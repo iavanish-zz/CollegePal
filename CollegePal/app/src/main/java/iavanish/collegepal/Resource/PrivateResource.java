ff2824238a617ab0a2a7a52d4b164cb7cce59976
iavanish <avanish1404@iiitd.ac.in>
2015-03-27 22:39:11 +0530

    Some Progress

---
+++ CollegePal/app/src/main/java/iavanish/collegepal/Resource/PrivateResource.java
@@ -0,0 +1,20 @@
+
+package iavanish.collegepal.Resource;
+
+/**
+ *
+ * @author iavanish
+ */
+
+/**
+ * 
+ * Private resources for a course
+ */
+
+public class PrivateResource extends Resource {
+    
+    public PrivateResource() {
+        super();
+    }
+    
+}
---
+++ CollegePal/app/src/main/java/iavanish/collegepal/Resource/PublicPrivateResource.java
@@ -0,0 +1,19 @@
+
+package iavanish.collegepal.Resource;
+
+/**
+ *
+ * @author iavanish
+ */
+
+/**
+ * 
+ * Public resource or Private Resource
+ */
+
+public enum PublicPrivateResource {
+    
+    PUBLIC,
+    PRIVATE
+    
+};
