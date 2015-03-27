eaf8fe645d677ea22dabfaba0f0670bdb1577c24
deepraghu1c <deependra1437@iiitd.ac.in>
2015-03-27 22:45:32 +0530

    Some Progress

---
+++ CollegePal/app/src/main/java/iavanish/collegepal/DiscussionForum/NewComment.java
@@ -0,0 +1,30 @@
+
+package iavanish.collegepal.DiscussionForum;
+
+import iavanish.collegepal.CommonClasses.DataBaseWrite;
+
+/**
+ *
+ * @author deependra
+ */
+
+/**
+ * 
+ * New comment
+ */
+
+public class NewComment {
+      
+    private Comment comment;
+    
+    private DataBaseWrite dataBaseWrite;
+    
+    public NewComment(Comment comment) {
+        this.comment = comment;
+    }
+    
+    public void createNewComment() {
+        
+    }
+    
+}
---
+++ CollegePal/app/src/main/java/iavanish/collegepal/DiscussionForum/NewDiscussion.java
@@ -0,0 +1,30 @@
+
+package iavanish.collegepal.DiscussionForum;
+
+import iavanish.collegepal.CommonClasses.DataBaseWrite;
+
+/**
+ *
+ * @author deependra
+ */
+
+/**
+ * 
+ * Start a new discussion
+ */
+
+public class NewDiscussion {
+    
+    private Discussion discussion;
+    
+    private DataBaseWrite dataBaseWrite;
+    
+    public NewDiscussion(Discussion discussion) {
+        this.discussion = discussion;
+    }
+    
+    public void createNewDiscussion() {
+        
+    }
+    
+}
