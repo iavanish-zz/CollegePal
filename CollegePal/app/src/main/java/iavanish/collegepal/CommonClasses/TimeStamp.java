f9df98213c5c1f2c9a284bbb87f6e5bec7f43a0d
iavanish <avanish1404@iiitd.ac.in>
2015-03-27 22:34:21 +0530

    Some Progress

---
+++ CollegePal/app/src/main/java/iavanish/collegepal/CommonClasses/TimeStamp.java
@@ -0,0 +1,25 @@
+
+package iavanish.collegepal.CommonClasses;
+
+/**
+ *
+ * @author iavanish
+ */
+
+/**
+ * 
+ * Time stamp
+ */
+
+public class TimeStamp {
+    
+    public Date date;
+    public int hours;
+    public int minutes;
+    public int seconds;
+    
+    public TimeStamp() {
+        
+    }
+    
+}
