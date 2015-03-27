01f078162795447b23471f6e6b63430f88c397f2
iavanish <avanish1404@iiitd.ac.in>
2015-03-27 22:32:08 +0530

    Some Progress

---
+++ CollegePal/app/src/main/java/iavanish/collegepal/Deadlines/SuggestedActivities.java
@@ -0,0 +1,29 @@
+
+package iavanish.collegepal.Deadlines;
+
+import iavanish.collegepal.CommonClasses.Date;
+import iavanish.collegepal.CommonClasses.Student;
+import iavanish.collegepal.CommonClasses.Course;
+
+/**
+ *
+ * @author iavanish
+ */
+
+/**
+ * 
+ * Suggested Activities (Schedule)
+ */
+
+public class SuggestedActivities {
+    
+    Student student;
+    Date date;
+    Course course;
+    String activity;
+    
+    public SuggestedActivities() {
+        
+    }
+    
+}
