95a438140fac13f8b4aae87284463c3bf6a7d9b9
iavanish <avanish1404@iiitd.ac.in>
2015-03-27 22:40:12 +0530

    Some Progress

---
+++ CollegePal/app/src/main/java/iavanish/collegepal/Resource/TypeOfResource.java
@@ -0,0 +1,20 @@
+
+package iavanish.collegepal.Resource;
+
+/**
+ *
+ * @author iavanish
+ */
+
+/**
+ * 
+ * Type of resource
+ */
+
+public enum TypeOfResource {
+    
+    NOTES,
+    SNAPSHOT,
+    RESOURCE
+    
+};
